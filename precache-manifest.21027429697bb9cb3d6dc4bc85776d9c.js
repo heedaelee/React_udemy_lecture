2c31af0e8f927c65b6b741cc1baea7feb7f395d5
David <adguy72@gmail.com>
2020-02-15 20:46:44 +0900

    Updates

---
+++ precache-manifest.21027429697bb9cb3d6dc4bc85776d9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a42d9a9f01ecc97f67f07a99f05171",
+    "url": "/react_udemy_lecture/index.html"
+  },
+  {
+    "revision": "a3a77fb067fe5dbdf85d",
+    "url": "/react_udemy_lecture/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "33a7450dda356927c657",
+    "url": "/react_udemy_lecture/static/css/main.ce2f89f8.chunk.css"
+  },
+  {
+    "revision": "a3a77fb067fe5dbdf85d",
+    "url": "/react_udemy_lecture/static/js/2.0ae71969.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react_udemy_lecture/static/js/2.0ae71969.chunk.js.LICENSE"
+  },
+  {
+    "revision": "33a7450dda356927c657",
+    "url": "/react_udemy_lecture/static/js/main.184873fe.chunk.js"
+  },
+  {
+    "revision": "25db8168cc9f66859ce6",
+    "url": "/react_udemy_lecture/static/js/runtime-main.b5b021a1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react_udemy_lecture/static/media/SEGA.12a3db8b.woff"
+  }
+]);
